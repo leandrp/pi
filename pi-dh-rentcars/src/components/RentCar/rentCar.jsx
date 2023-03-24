e5c72c5206ff1d72354fc488faa4dac84a1b3176
leandrp <ferreira.leandro.p@gmail.com>
2023-03-23 23:03:17 -0300

    estilização rentCar

--- pi-dh-rentcars/src/components/RentCar/rentCar.jsx
+++ pi-dh-rentcars/src/components/RentCar/rentCar.jsx
@@ -20,7 +20,6 @@ import { useState } from 'react'
 import { RentCarForm } from '../RentCarForm/rentCarForm'
 
 export function RentCar() {
-
   const navigate = useNavigate()
 
   const handleStartReservation = () => {
@@ -48,68 +47,80 @@ export function RentCar() {
           </button>
         </div>
 
-        <div className="rent-car-det-evaluation">
-          <p className="rent-car-det-description">
-            <FaMapMarkerAlt /> {car.description}
-          </p>
-          <div className="rent-car-det-rating">
-            <>
-              <span className="rent-car-det-star">&#9733;&#9733;&#9733;</span>
-              {Array(car.rating)
-                .fill()
-                .map((_, id) => (
-                  <span key={id} className="rent-car-det-star">
-                    &#9733;
-                  </span>
-                ))}
-              <div className="rent-car-det-note">{car.note}</div>
-              <p className="rent-car-det-note-text">{car.notetext}</p>
-            </>
+        <div className="rent-car-det-text">
+          <div className="rentcar-colum-1-content">
+            <div className="rent-car-det-text-content">
+              <h2 className="rent-car-det-text-title">Complete seus dados </h2>
+              <RentCarForm />
+            </div>
+            <div className="calendar">
+              <h2 className="calendar-text">
+                Selecione sua data e horário de reserva
+              </h2>
+              <DateRangePicker
+                disabledDate={beforeToday()}
+                format="dd-MM-yyyy HH:mm"
+                locale={{
+                  sunday: 'Dom',
+                  monday: 'Seg',
+                  tuesday: 'Ter',
+                  wednesday: 'Qua',
+                  thursday: 'Qui',
+                  friday: 'Sex',
+                  saturday: 'Sab',
+                  ok: 'Aplicar',
+                  today: 'Hoje',
+                  yesterday: 'Ontem',
+                  hours: 'Horas',
+                  minutes: 'Minutos'
+                }}
+                placeholder="Data e Hora: Retidada | Devolução"
+                open={true}
+                className="custom-date-range-picker"
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="rent-car-det-img-block" key={car.id}>
-          <div className="rent-car-det-img">
-            <div className="rent-car-det-img-wrapper-1">
-              <img src={car.image} alt={car.alt} />
+          <div className="rentcar-colum-2-content">
+            <div className="rentcar-colum-2-content-card">
+              <h2 className="rent-car-det-text-title">Detalhes da reserva</h2>
+              <div className="rent-car-det-img-block" key={car.id}>
+                <div className="rent-car-det-img">
+                  <div className="rent-car-det-img-wrapper-1">
+                    <img src={car.image} alt={car.alt} />
+                  </div>
+                </div>
+              </div>
+              <div className="rent-car-det-header-card">
+                <span className="rent-car-det-header-text-rating">CARRO </span>
+                <h2 className="rent-car-det-header-title">{car.name} </h2>
+              </div>
+              <div className="rent-car-det-rating">
+                <>
+                  <span className="rent-car-det-star"></span>
+                  {Array(car.rating)
+                    .fill()
+                    .map((_, id) => (
+                      <span key={id} className="rent-car-det-star">
+                        &#9733;
+                      </span>
+                    ))}
+                </>
+              </div>
+              <div className="rent-car-det-evaluation">
+                <p className="rent-car-det-description">{car.description}</p>
+              </div>
+              <div className="horizontal-line-card"></div>
+              <div className="horizontal-line-card"></div>
+              <div className="horizontal-line-card"></div>
+              <button className="btn-reserva" onClick={handleStartReservation}>
+                Iniciar reserva
+              </button>
             </div>
           </div>
         </div>
-        <button className="btn-reserva" onClick={handleStartReservation}>
-          Iniciar reserva
-        </button>
-        <div className="rent-car-det-text">
-          <div className="rent-car-det-text-content">
-            <h2 className="rent-car-det-text-title">Complete seus dados </h2>
-                  <RentCarForm />
-          </div>
-          <div className="calendar">
-            <h2 className="calendar-text">
-              Selecione sua data e horário de reserva
-            </h2>
-            <DateRangePicker
-              disabledDate={beforeToday()}
-              format="dd-MM-yyyy HH:mm"
-              locale={{
-                sunday: 'Dom',
-                monday: 'Seg',
-                tuesday: 'Ter',
-                wednesday: 'Qua',
-                thursday: 'Qui',
-                friday: 'Sex',
-                saturday: 'Sab',
-                ok: 'Aplicar',
-                today: 'Hoje',
-                yesterday: 'Ontem',
-                hours: 'Horas',
-                minutes: 'Minutos'
-              }}
-              placeholder="Data e Hora: Retidada | Devolução"
-              open={true}
-              className="custom-date-range-picker"
-            />
-          </div>
 
+        <div className="rentcar-footer-rules">
           <div className="rules">
             <h2 className="rules-title">O que você precisa saber:</h2>
             <div className="horizontal-line"></div>
